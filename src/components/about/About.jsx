7d6ee061eb53f21570c7e9dda4e426ddf885eb87
veralibitoye <veracity@esettlementgroup.com>
2023-11-06 13:48:42 +0100

    first commit

---
+++ src/components/about/About.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col left d-flex justify-content-center p-5">
+          <img
+            className="aboutimage"
+            src="https://images.pexels.com/photos/3288103/pexels-photo-3288103.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+          />
+        </div>
+        <div className="col right d-flex justify-content-center p-5 flex-column">
+          <h3 className=" text-center text-decoration-underline">
+            About Vera real estate
+          </h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro
+            necessitatibus culpa libero enim laudantium repellat, doloremque
+            soluta cupiditate? Quod reiciendis dolorem laborum ut molestiae
+            quis? Debitis molestias inventore magnam quos minus pariatur
+            blanditiis doloremque, eveniet provident facere possimus nam
+            laboriosam sunt soluta velit reiciendis natus, aperiam eaque
+            distinctio dolorem sit asperiores. Minima nobis, asperiores
+            dignissimos sapiente quis ex repudiandae. Vitae fugit nemo nam ipsam
+            similique fugiat totam veritatis! Perspiciatis sapiente est odio
+            atque voluptatibus voluptates! Natus rem modi enim dolorem.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
